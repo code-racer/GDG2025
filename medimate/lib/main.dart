197acd7b3dd82d5defd0c2b44936a1358410636f
Aswin_Oommen_Jacob <aswin@ethiccoders.com>
2025-06-04 20:11:02 +0530

    main page error fixed!

--- medimate/lib/main.dart
+++ medimate/lib/main.dart
@@ -9,11 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello, World!', style: TextStyle(fontSize: 24)),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello, World!', style: TextStyle(fontSize: 24)),
+        ),
+        backgroundColor: Colors.blueAccent,
       ),
-      backgroundColor: Colors.blueAccent,
     );
   }
 }
